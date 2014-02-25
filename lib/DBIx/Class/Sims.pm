f07fd8c90315e47f2a2f1a6360c29f4054f3f613
Rob Kinyon <rob.kinyon@gmail.com>
2014-02-25 16:48:32 +0000

    Import of RKINYON/DBIx-Class-Sims-0.200020 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Sims
    gitpan-cpan-version:      0.200020
    gitpan-cpan-path:         RKINYON/DBIx-Class-Sims-0.200020.tar.gz
    gitpan-cpan-author:       RKINYON
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Sims.pm
+++ lib/DBIx/Class/Sims.pm
@@ -7,11 +7,12 @@ use strict;
 use warnings FATAL => 'all';
 
 use Data::Walk qw( walk );
+use Hash::Merge qw( merge );
 use List::Util qw( shuffle );
 use Scalar::Util qw( reftype );
 use String::Random qw( random_regex );
 
-our $VERSION = '0.200010';
+our $VERSION = '0.200020';
 
 # Guarantee that toposort is loaded.
 use base 'DBIx::Class::TopoSort';
@@ -44,6 +45,19 @@ use base 'DBIx::Class::TopoSort';
   BEGIN { *sim_types = \&sim_type; }
 }
 use DBIx::Class::Sims::Types;
+
+sub add_sim {
+  my $class = shift;
+  my ($schema, $source, $column, $sim_info) = @_;
+
+  my $col_info = $schema->source($source)->column_info($column);
+  $col_info->{sim} = merge(
+    $col_info->{sim} // {},
+    $sim_info,
+  );
+
+  return;
+}
 
 sub load_sims {
   my $self = shift;
